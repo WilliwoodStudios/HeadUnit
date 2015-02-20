34bfb663b07ce0b7aae39e383a558fa2ed6a2867
Tim Neil <tim_neil@hotmail.com>
2015-02-20 11:12:52 -0500

    added in app code

---
+++ phonegap/brainiac/www/js/chrome.js
@@ -0,0 +1,8 @@
+function screenChrome() {
+	this.component = ws12.HeadUnitChrome;
+	this.home = paneMenu;
+	
+	this.onshow = function() {
+		
+	}
+}
---
+++ phonegap/brainiac/www/js/pane-Menu.js
@@ -0,0 +1,69 @@
+function paneMenu() {
+	this.component = ws12.WindowPane;
+	
+	this.disableAnimation = true;
+	
+	this.background = {
+		img: 'img/background.jpg',
+		colorized: true,
+		position: 'center bottom',
+	};
+	
+	this.content = [
+		{
+			component: ws12.CircleMenu,
+			items: [
+				{
+					caption: 'music',
+					imgClass: 'ws12-icon-head-unit-music',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+					}
+				},
+				{
+					caption: 'maps',
+					imgClass: 'ws12-icon-head-unit-map',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+					}
+				},
+				{
+					caption: 'browser',
+					imgClass: 'ws12-icon-head-unit-browser',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+						this.screen.container.push(paneBrowser);
+					}
+				},
+				{
+					caption: 'phone',
+					imgClass: 'ws12-icon-head-unit-phone',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+					}
+				},
+				{
+					caption: 'dashboard',
+					imgClass: 'ws12-icon-head-unit-dashboard',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+						this.screen.container.push(paneDashboard);
+					}
+				},
+				{
+					caption: 'logging',
+					imgClass: 'ws12-icon-head-unit-logging',
+					onclick: function() {
+						console.log(this.caption + ' clicked');
+					}
+				}
+				
+			]
+		}
+	
+	];
+	
+	this.onshow = function() {
+		
+	}
+}
