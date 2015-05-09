fa5219a9c579ca4bfa7d263d67b07d35acf90888
Rob Williams Jnr <rob@workshoptwelve.com>
2015-05-08 21:31:04 -0400

    Adding logging to main activity.

--- java/Boss/app/src/main/java/com/workshoptwelve/boss/app/log/AndroidLogger.java
+++ java/Boss/app/src/main/java/com/workshoptwelve/boss/app/log/AndroidLogger.java
@@ -2,37 +2,49 @@ package com.workshoptwelve.boss.app.log;
 
 import android.util.Log;
 
-import com.workshoptwelve.brainiac.boss.common.log.Logger;
 import com.workshoptwelve.brainiac.boss.common.log.Log.Level;
+import com.workshoptwelve.brainiac.boss.common.log.Logger;
 
 /**
  * Created by robwilliams on 15-05-05.
  */
 public class AndroidLogger extends Logger {
     private final String mTag;
+    private AndroidLoggerListener mListener;
 
     public AndroidLogger(String tag) {
         mTag = tag;
     }
-    
+
+    public void setAndroidLoggerListener(AndroidLoggerListener listener) {
+        mListener = listener;
+    }
+
     @Override
     protected void rawLog(Level level, StringBuilder toLog) {
-        switch(level) {
+        switch (level) {
             case d:
-                Log.d(mTag,toLog.toString());
+                Log.d(mTag, toLog.toString());
                 break;
             case i:
-                Log.e(mTag,toLog.toString());
+                Log.i(mTag, toLog.toString());
                 break;
             case v:
-                Log.v(mTag,toLog.toString());
+                Log.v(mTag, toLog.toString());
                 break;
             case w:
-                Log.w(mTag,toLog.toString());
+                Log.w(mTag, toLog.toString());
                 break;
             case e:
-                Log.e(mTag,toLog.toString());
+                Log.e(mTag, toLog.toString());
                 break;
+        }
+        try {
+            if (mListener != null) {
+                mListener.onLogAvailable(level, toLog);
+            }
+        } catch (Exception e) {
+
         }
     }
 
@@ -40,5 +52,9 @@ public class AndroidLogger extends Logger {
     protected void getPrefix(Level level, StringBuilder prefix) {
         getTrace(prefix);
         prefix.append(": ");
+    }
+
+    public interface AndroidLoggerListener {
+        void onLogAvailable(Level level, StringBuilder toLog);
     }
 }
