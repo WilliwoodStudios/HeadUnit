69e2c68d4e5b52102406ae5656ed9302feaabdda
Rob Williams Jnr <rob@workshoptwelve.com>
2015-05-27 11:43:08 -0400

    Adding new screens.

---
+++ emulator/apps/core.media.player/js/artistList.js
@@ -0,0 +1,111 @@
+function artistList() {
+    this.component = $ui.WindowPane;
+    this.animated = true;
+    this.backCaption = 'My Library',
+    
+    this.content = [
+        {
+            component: $ui.ControlGroup,
+            visible: false,
+            id: 'topPlayed',
+            content: [
+                {
+                    component: $ui.Header,
+                    caption: 'Top Played Artists'
+                },
+                {
+                    component: $ui.List,
+                    style: $ui.ImageListItem,
+                    provider: {
+                        id: 'topPlayedProvider',
+                        property: 'items'
+                    },
+                    onaction: function(event) {
+                        event.target.sharedDetailMode = "artist";
+                        $ui.push(sharedDetail,event.target);
+                    }
+                }
+            ]
+        },
+        {
+            component: $ui.List,
+            style: $ui.GenericListItem,
+            provider: {
+                id: 'entryProvider',
+                property: 'items'
+            },
+            onaction: function(event) {
+                event.target.sharedDetailMode = "artist";
+                $ui.push(sharedDetail, event.target);
+            }
+        }
+    
+    ];
+    
+    this.attachedObjects = [
+        {
+            component: $ui.DataProvider,
+            id: 'entryProvider',
+            onbeforeupdate: function() {
+                var i,
+                    item,
+                    firstLetter,
+                    letters = [],
+                    header;
+                // Rename any necessary fields for display in the list
+                // and insert letter headers
+                for (i = 0; i < this.data.items.length; i++) {
+                    item = this.data.items[i];
+                    item.title = item.name;
+                    item.img = item.artwork;
+                    item.accent = item.albumCount + " " + (item.albumCount==1 ? "Album" : "Albums");
+                    item.accent += " - ";
+                    item.accent += item.songCount + " " + (item.songCount==1 ? "Song" : "Songs");
+                    item.accent += " - ";
+                    item.accent += item.playCount + " " + (item.playCount==1 ? "Play" : "Plays");
+                    item.img = item.artwork;
+                    if (item.title && item.title.length > 0) {
+                        firstLetter = item.title[0].toUpperCase();
+                        if (letters.indexOf(firstLetter) < 0) {
+                            letters.push(firstLetter);
+                            header = {
+                                component: $ui.Header,
+                                caption: firstLetter
+                            };
+                            this.data.items.splice(i,0,header);
+                        }
+                    }
+                }
+            }
+        },
+        {
+            component: $ui.DataProvider,
+            id: 'topPlayedProvider',
+            onbeforeupdate: function() {
+                var i,
+                    item;
+                // Rename any necessary fields for display in the image list
+                for (i = 0; i < this.data.items.length; i++) {
+                    item = this.data.items[i];
+                    item.caption = item.name;
+                    item.img = item.artwork;
+                }
+            }
+        }
+    ];
+    
+    this.onshow = function() {
+        var mediaSource = $system.audio.getActiveMediaSource();
+        if (mediaSource) {
+            // Get our top played albums
+            var topPlayed = mediaSource.getMostPlayedArtists();
+            if (topPlayed && (topPlayed.length > 0)) {
+                this.topPlayedProvider.data = {items: topPlayed};
+                this.topPlayed.visible = true;
+            }
+            // Retrieve our list of all atrists
+            var artists = mediaSource.getArtists();
+            this.entryProvider.data = {items: artists};
+        }
+    }
+}
---
+++ emulator/apps/core.media.player/js/sharedDetail.js
@@ -0,0 +1,157 @@
+function sharedDetail() {
+    function getFirst(value) {
+        value = value.toUpperCase();
+        value = value.replace(/^\"/g, "");
+        value = value.replace(/^\'/g, "");
+        return value.charAt(0);
+    }
+
+    this.component = $ui.WindowPane;
+    this.animated = true;
+    this.backCaption = "Set in oncreate...";
+    
+    /**
+     * The mode that this sreen will present data in.
+     */
+    this.viewMode = "Set in oncreate...";
+
+    this.content = [{
+        component: $ui.List,
+        style: $ui.GenericListItem,
+        provider: {
+            id: 'entryProvider',
+            property: 'items'
+        },
+        onaction: function (event) {
+            if (event.target && event.target.startPlaying) {
+                event.target.startPlaying();
+                if (this.viewMode === "song") {
+                    $ui.pop(2);
+                } else {
+                    $ui.pop(3);
+                }
+            } else {
+                $ui.pop();
+            }
+        }.$bind(this)
+    }];
+
+    this.attachedObjects = [{
+        component: $ui.DataProvider,
+        id: 'entryProvider',
+        onbeforeupdate: function () {
+            var i,
+                item,
+                firstLetter,
+                lastAlbum;
+            lastAlbum = null;
+
+            for (i = 0; i < this.data.items.length; i++) {
+                item = this.data.items[i];
+                if (item.component === $ui.Header) {
+                    continue;
+                }
+
+                item.title = "";
+
+                item.title += item.name;
+
+                item.accent = $system.util.converter.secondsToTime(item.duration);
+
+                if (item.accent.length > 0) {
+                    item.accent += " - ";
+                }
+                if (item.playCount) {
+                    item.accent += item.playCount + " " + (item.playCount == 1 ? "Play" : "Plays");
+                }
+
+                item.img = item.artwork;
+
+                if (this.viewMode === "genre" || this.viewMode === "song") {
+                    item.caption = item.albumName + " - " + item.artistName;
+                    if (this.data.items.length > 15) {
+                        if (item.name.length > 1) {
+                            var first = getFirst(item.name);
+                            if (first != firstLetter) {
+                                firstLetter = first;
+                                this.data.items.splice(i, 0, {
+                                    component: $ui.Header,
+                                    caption: firstLetter
+                                });
+                            }
+                        }
+                    }
+                } else {
+                    if (item.albumName != lastAlbum) {
+                        this.data.items.splice(i, 0, {
+                            component: $ui.Header,
+                            caption: item.albumName + " - " + item.artistName
+                        });
+                        lastAlbum = item.albumName;
+                    }
+                }
+            }
+        }
+    }];
+    
+    /** 
+     * Set the correct backCaption for the viewMode we are in.
+     */
+    this.setBackCaption = function() {
+        if (this.viewMode == "artist") {
+            this.backCaption = "Artists";
+        } else if (this.viewMode == "album") {
+            this.backCaption = "Albums";
+        } else if (this.viewMode == "genre") {
+            this.backCaption = "Genres";
+        } else {
+            this.backCaption = "Back";
+        }
+    }.$bind(this);
+    
+    /**
+     * item.sharedDetailMode makes this screen behave correctly.
+     * @param item.sharedDetailMode
+     */
+    this.oncreate = function (item) {
+        console.log("oncreate " + item);
+        if (item && item.sharedDetailMode) {
+            this.viewMode = item.sharedDetailMode;
+        } else {
+            console.log("Setting to default - song");
+            this.viewMode = "song";
+        }
+        this.setBackCaption();
+    }.$bind(this);
+
+    this.onshow = function (item) {
+        var mediaSource = $system.audio.getActiveMediaSource();
+        if (mediaSource) {
+            var songs = [];
+            var emptyMessage = "";
+            
+            if (this.viewMode == "album") {
+                songs = mediaSource.getAlbumSongs(item.uid);
+                emptyMessage = "No songs in the sample album...";
+            } else if (this.viewMode == "artist") {
+                songs = mediaSource.getArtistSongs(item.uid);
+                emptyMessage = "No songs for this sample artist...";
+            } else if (this.viewMode == "genre") {
+                songs = mediaSource.getGenreSongs(item.uid);
+                emptyMessage = "No songs for this sample genre...";
+            } else if (this.viewMode === "song") {
+                songs = mediaSource.getSongs();
+                emptyMessage = "No songs in the library...";
+            }
+            
+            if (songs.length == 0) {
+                songs.push({
+                    name: emptyMessage
+                });
+            }
+            this.entryProvider.data = {
+                items: songs
+            };
+        }
+    }.$bind(this);
+}
