f400d9bbfef6163d3f375d527b7078c94c596f77
Rob Williams Jnr <rob@workshoptwelve.com>
2015-05-28 00:10:57 -0400

    Adding shuffle / repeat.

--- emulator/apps/core.media.player/js/main.js
+++ emulator/apps/core.media.player/js/main.js
@@ -8,60 +8,103 @@ function main() {
 				id: 'mediaPlayerProvider',
 				property: 'currentSong'
 			},
-			onmenuclick: function() {
+			onmenuclick: function () {
 				$ui.push(libraryMenu);
 			},
-			onplay: function() {
+			onplay: function () {
 				var mediaSource = $system.audio.getActiveMediaSource();
 				if (mediaSource && (mediaSource.type == $system.MediaSourceType.PLAYER)) {
 					mediaSource.play();
 				}
 			},
-			onpause: function() {
+			onpause: function () {
 				var mediaSource = $system.audio.getActiveMediaSource();
 				if (mediaSource && (mediaSource.type == $system.MediaSourceType.PLAYER)) {
 					mediaSource.pause();
 				}
 			},
-			onskipback: function() {
+			onskipback: function () {
 				var mediaSource = $system.audio.getActiveMediaSource();
 				if (mediaSource && (mediaSource.type == $system.MediaSourceType.PLAYER)) {
 					mediaSource.skipBack();
 				}
 			},
-			onskipforward: function() {
+			onskipforward: function () {
 				var mediaSource = $system.audio.getActiveMediaSource();
 				if (mediaSource && (mediaSource.type == $system.MediaSourceType.PLAYER)) {
 					mediaSource.skipForward();
 				}
+			},
+			ontoggleshuffle: function() {
+				$system.songHistory.setShuffle(!$system.songHistory.getShuffle());
+			},
+			ontogglerepeat: function() {
+				var repeat = $system.songHistory.getRepeat();
+				repeat += 1;
+				repeat %= 3;
+				$system.songHistory.setRepeat(repeat);
 			}
 
-		}	
+		}
 	];
-	
+
 	this.attachedObjects = [
 		{
 			component: $ui.DataProvider,
 			id: 'mediaPlayerProvider'
 		}
 	];
-	
-	this.onshow = function() {
-		// Set our current media
+
+	this.onCurrentSongChanged = function (event) {
+		// pretty much ignoring the event...
 		var mediaSource = $system.audio.getActiveMediaSource();
+		var data = {
+			currentSong: {
+				album: "-",
+				covertArt: "",
+				song: "",
+				artist: "",
+				duration: "0:00",
+				paused: true
+			}
+		};
 		if (mediaSource && (mediaSource.type == $system.MediaSourceType.PLAYER)) {
-			var song = mediaSource.getCurrentSong(),
+			var song = mediaSource.getCurrentSong();
+			if (song) {
 				data = {
 					currentSong: {
 						album: song.albumName,
-						coverArt: song.albumArt,
+						coverArt: song.artwork,
 						song: song.name,
 						artist: song.artistName,
 						duration: song.duration,
 						paused: mediaSource.isPaused()
 					}
 				};
-			this.mediaPlayerProvider.data = data;
+			}
 		}
-	}
+		this.mediaPlayerProvider.data = data;
+	}.$bind(this);
+	
+	var self = this;
+	
+	this.onShuffleChanged = function(event) {
+		self.mediaPlayer.setShuffle(event.data);
+	};
+	
+	this.onRepeatChanged = function(event) {
+		self.mediaPlayer.setRepeat(event.data);
+	};
+
+	this.onshow = function () {
+		// Set our current media
+		this.onCurrentSongChanged();
+		console.log("Going to add event listener");
+		console.log("$data: " + $data);
+		
+		$data.addEventListener($system.songHistory.MEDIA_SONG_CHANGED, this.onCurrentSongChanged);
+		$data.addEventListener($system.songHistory.MEDIA_SHUFFLE_CHANGED,this.onShuffleChanged);
+		$data.addEventListener($system.songHistory.MEDIA_REPEAT_CHANGED,this.onRepeatChanged);
+	};
+
 }
