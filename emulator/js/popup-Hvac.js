9727ec48c9fbedb1c4da6b4d823a46e4e3fba30b
Rob Williams Jnr <rob@workshoptwelve.com>
2015-06-04 14:43:23 -0400

    Adding popup hvac screen.

---
+++ emulator/js/popup-Hvac.js
@@ -0,0 +1,15 @@
+function popupHvac() {
+	this.component = $ui.PopupHvac;
+
+//	
+//	this.backButton = {
+//		icon: 'emulator-icon-head-unit-temperature',
+//		caption: 'Done'
+//	};
+//	
+	this.onchange = function() {
+	};
+	
+	this.onshow = function(data) {
+	};
+}
