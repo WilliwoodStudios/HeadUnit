f304c39881c064e11f13c3280e8f827c3053e348
Rob Williams Jnr <rob@workshoptwelve.com>
2015-06-05 11:34:19 -0400

    Removed redundant properties.

--- emulator/js/popup-Hvac.js
+++ emulator/js/popup-Hvac.js
@@ -1,15 +1,4 @@
 function popupHvac() {
 	this.component = $ui.PopupHvac;
-
-//	
-//	this.backButton = {
-//		icon: 'emulator-icon-head-unit-temperature',
-//		caption: 'Done'
-//	};
-//	
-	this.onchange = function() {
-	};
-	
-	this.onshow = function(data) {
-	};
+    this.animated = true;
 }
