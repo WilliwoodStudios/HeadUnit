f33f84b9d6c3939e5c118807598c8cf21dd4f10d
tneil <tim_neil@hotmail.com>
2015-07-17 11:07:21 -0400

    added initial app for suspension #67

---
+++ emulator/apps/core.suspension/js/index.js
@@ -0,0 +1,17 @@
+/* Copyright (c) 2015 Workshop 12 Inc. */
+var $system;
+// Initialize our toolkit
+window.onload = function() {
+	// Get the system object
+	if (!$system) {
+		if (window.location !== window.parent.location) {
+			// We are in the emulator
+			$system = window.parent.$system;
+		} else {
+			console.log('WARNING: $system not defined');
+		}
+	} 
+	$system.registerApp('core.suspension', $ui);
+	// Initialize
+	$ui.init(main, $system.config.theme);	
+}
---
+++ emulator/apps/core.suspension/js/main.js
@@ -0,0 +1,11 @@
+/* Copyright (c) 2015 Workshop 12 Inc. */
+function main() {
+	this.component = $ui.WindowPane;
+
+	this.content = [];
+
+	this.onshow = function() {
+		//$ui.addEventListener('relay_switch_config_change', this.onswitchchange, this);
+		//$system.relays.getRelayList(this.onrelaylistload);
+	}
+}
