9322fce9e1cf902a340ca924981f39f06750814a
tneil <tim_neil@hotmail.com>
2015-07-20 11:56:18 -0400

    updates for issue #67

--- emulator/apps/core.suspension/js/main.js
+++ emulator/apps/core.suspension/js/main.js
@@ -4,14 +4,74 @@ function main() {
 
 	this.content = [
 		{
-			component: $ui.Suspension,
-			leftFront: 21,
-			rightFront: 25,
-			leftRear: 30,
-			rightRear: 30,
-			tank: 112,
-			img: 'img/cartop.png'
+			component: $ui.ColumnLayout,
+			fillToParent: true,
+			columns: [
+				{
+					component: $ui.Column,
+					content: [
+						{
+							component: $ui.DockLayout,
+							location: $ui.DockLayout.DockLocation.BOTTOM,
+							dock: [
+								{
+						            component: $ui.Button,
+						            caption: 'Add Preset'
+						        }
+							],
+							content: [
+								{
+									component: $ui.List,
+									style: $ui.SuspensionListItem,
+									items: [
+										{
+											component: $ui.Header,
+											caption: 'Height Presets'	
+										},
+										{
+											caption: 'Parked'
+										},
+										{
+											caption: 'Driving'
+										},
+										{
+											caption: 'Full Height'
+										},
+										{
+											caption: 'My Driveway',
+											style: 'location'
+										},
+										{
+											caption: 'Over 90 km/h',
+											style: 'speed'
+										}
+									],
+									onaction: function(event) {
+										
+									}
+								}
+							]
+						}
+					]
+				},
+				{
+					component: $ui.Column,
+					span: 2,
+					content: [
+						{
+							component: $ui.Suspension,
+							leftFront: 21,
+							rightFront: 25,
+							leftRear: 30,
+							rightRear: 30,
+							tank: 112,
+							img: 'img/cartop.png'
+						}
+					]
+				}
+			]
 		}
+		
 	];
 
 	this.onshow = function() {
