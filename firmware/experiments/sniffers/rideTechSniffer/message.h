f8635416e79d1cdd92365d948082af865413eff8
Rob Williams <rob@workshoptwelve.com>
2015-07-23 11:12:15 -0400

    Adding a dedicated sniffer project.

---
+++ firmware/experiments/sniffers/rideTechSniffer/message.h
@@ -0,0 +1,87 @@
+#ifndef _MESSAGE_H
+#define _MESSAGE_H
+
+#include <Arduino.h>
+
+#define MESSAGE_LIST_SIZE 20
+
+class Message {
+public:
+    Message() {
+        mCount = -1;
+        mA = 0;
+        mB = 0;
+    }
+
+    virtual ~Message() {
+    }
+
+    void operator=(Message & message) {
+        mCount = message.mCount;
+        mA = message.mA;
+        mB = message.mB;
+    }
+
+    bool operator>(Message & message) {
+        return mCount > message.mCount;
+    }
+
+    void setMessage(uint8_t a, uint8_t b) {
+        if (a==mA && b==mB) {
+            return;
+        }
+        mA = a;
+        mB = b;
+        mCount = 0;
+    }
+
+    void operator++() {
+        ++mCount;
+    }
+
+    int32_t getCount() {
+        return mCount;
+    }
+
+    void dump() {
+        if (mA < 16) {
+            Serial.print("0");
+        }
+        Serial.print(mA,HEX);
+        Serial.print(" ");
+        if (mB < 16) {
+            Serial.print("0");
+        }
+        Serial.print(mB,HEX);
+        Serial.print(" ");
+        Serial.println(mCount);
+    }
+
+private:
+    uint8_t mA;
+    uint8_t mB;
+
+    int32_t mCount;
+};
+
+class MessageList {
+public:
+    MessageList() {
+    }
+    virtual ~MessageList() {
+    }
+
+    void addMessage(uint8_t a, uint8_t b);
+
+    void dump();
+
+    void sort();
+
+    void randomize();
+
+private:
+    Message messages[MESSAGE_LIST_SIZE];
+};
+
+
+#endif
