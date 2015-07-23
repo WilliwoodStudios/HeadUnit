c477b763b96961385ced5df8c4d66ad17a4c5e0a
Rob Williams <rob@workshoptwelve.com>
2015-07-23 12:11:38 -0400

    Added help message.

--- firmware/suspension/rideTech/constants.cpp
+++ firmware/suspension/rideTech/constants.cpp
@@ -10,3 +10,13 @@ const PStr resultOK(_result_ok);
 
 const char _result_fail[] PROGMEM = "FAIL";
 const PStr resultFail(_result_fail);
+
+const char _string_help[] PROGMEM = "id - get device id\n"\
+                                    "g - get the current pressures\n"\
+                                    "    returns [<fl>,<fr>,<rl>,<rr>,<tank>]\n"\
+                                    "p <fl>,<fr>,<rl>,<rr> - seek to the given pressures\n" \
+                                    "v <valves> - set the valve positions - valves is in HEX\n" \
+                                    "s <n> - go to preset n\n"\
+                                    "m r|R|s|S - set the message mode\n"\
+                                    "? - display this help\n";
+const PStr stringHelp(_string_help);
