6b8e9299ef400390e31c54c065a25e928124a281
Rob Williams <rob@workshoptwelve.com>
2015-07-26 01:24:22 -0400

    Removed crosswalk experiment.

--- crosswalk/go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm *.apk
-CROSSWALK=/Users/robwilliams/Library/Android/crosswalk-14.43.343.17
-python $CROSSWALK/make_apk.py --app-versionCode=1 --package com.workshoptwelve.brainiac.ui --manifest=xwalk-simple/crosswalk.manifest.json --enable-remote-debugging
