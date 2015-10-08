64c99d5cbd427d9d16490431bacde0be6e3a3a73
Rob Williams <rob@workshoptwelve.com>
2015-10-08 09:03:28 -0400

    Added PID, removed provider.

--- emulator/apps/core.gauges/js/main.js
+++ emulator/apps/core.gauges/js/main.js
@@ -9,171 +9,48 @@ function main() {
 				{
 					component: $ui.RealTimeGauge,
 					mode: "speed",
-					provider: {
-
-					}
-				},
-				{
-					component: $ui.RealTimeGauge,
-					mode: "rpm",
-					provider: {
-
-					}
+					pid: 0xd
 				},
 				{
 					component: $ui.RealTimeGauge,
 					mode: "timingAdvance",
 					position: "top",
-					provider: {
-
-					}
+					pid: 0xe
+				},
+				{
+					component: $ui.RealTimeGauge,
+					mode: "rpm",
+					pid: 0xc
 				},
 				{
 					component: $ui.RealTimeGauge,
 					mode: "temperature",
 					title: "Air Intake",
 					position: "left",
-					provider: {
-
-					}
+					pid: 0xf
 				},
 				{
 					component: $ui.RealTimeGauge,
 					mode: "percentage",
 					title: "Throttle Position",
-					provider: {
-
-					}
+					pid: 0x11
 				},
 				{
 					component: $ui.RealTimeGauge,
 					mode: "temperature",
 					title: "Engine Oil",
-					provider: {
-
-					}
+					position: "right",
+					___pid: 0x5c
 				},
 				{
 					component: $ui.RealTimeGauge,
 					mode: "oxygenSensor",
-					position: "right"
+					position: "top"
 				}
-				/*
-				{
-					component: $ui.TileDistance,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'distanceInfo'
-					}
-				},
-				{
-					component: $ui.TileBadge,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'badgeInfo'
-					}
-				},
-				{
-					component: $ui.TileBraking,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'brakingInfo'
-					}
-				},
-				{
-					component: $ui.TileMPG,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'mpgInfo'
-					}
-				},
-				{
-					component: $ui.TileFuel,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'fuelInfo'
-					}
-				},
-				{
-					component: $ui.TileIdle,
-					animated: false,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'idleInfo'
-					}
-				},
-				{
-					component: $ui.TileIdleDetails,
-					animated: false,
-					provider: {
-						id: 'dashboardProvider',
-						property: 'idleDetailsInfo'
-					}
-				}
-				*/
 			]
-			// attachedObjects: [
-			
-			// 	{
-			// 		component: $ui.DataProvider,
-			// 		id: 'dashboardProvider'
-			// 	}
-				
-			// ]
 		}	
 	];
 	
 	this.onshow = function() {
-		// // Set the screen data
-		// var data = {
-		// 	profileInfo: {
-		// 		backgroundImg: "../img/subaru.jpg",
-		// 		avatar: "../img/avatar.png",
-		// 		userName: "brcewane",
-		// 		stats: {
-		// 			friends: 12,
-		// 			clubs: 6,
-		// 			score: 42369,
-		// 			rank: 2
-		// 		}			
-		// 	},
-		// 	idleInfo: { 
-		// 		value: 70
-		// 	},
-		// 	accelerationInfo: {
-		// 		min: 0,
-		// 		max: 1.5,
-		// 		value: 0.9
-		// 	},
-		// 	brakingInfo: {
-		// 		min: 0,
-		// 		max: 1.5,
-		// 		value: 0.3
-		// 	},
-		// 	mpgInfo: {
-		// 		max: 32,
-		// 		value: 20,
-		// 		abbreviation: 'MPG'
-		// 	},
-		// 	badgeInfo: {
-		// 		img: 'img/badge_idle_monochrome.png',
-		// 		caption: '<large>27</large>min commute',
-		// 		accent: 'Quickest trip to work!'
-		// 	},
-		// 	distanceInfo: {
-		// 		data: [20,25,9.2],
-		// 		units: 'miles'
-		// 	},
-		// 	fuelInfo: {
-		// 		value: 6,
-		// 		data: [90,50,30]
-		// 	},
-		// 	idleDetailsInfo: {
-		// 		labels: ['Sun','Mon','Tue','Wed','Thur','Fri','Today'],
-		// 		data: [0,35,17,65,0,10,15]
-		// 	}
-		// }
-		// // Populate the dashboard data provider
-		// this.dashboardProvider.data = data; 
-	}
+	};
 }
