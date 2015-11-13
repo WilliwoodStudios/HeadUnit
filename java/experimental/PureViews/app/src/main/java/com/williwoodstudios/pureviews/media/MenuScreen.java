4353f624d0e90dadc824a588dad786d0de230520
Rob Williams <rob@workshoptwelve.com>
2015-11-12 22:08:47 -0500

    Added circl menu to media menu screen.

--- java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/media/MenuScreen.java
+++ java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/media/MenuScreen.java
@@ -9,6 +9,10 @@ import android.widget.ImageView;
 import com.williwoodstudios.pureviews.AppScreen;
 import com.williwoodstudios.pureviews.R;
 import com.williwoodstudios.pureviews.Theme;
+import com.williwoodstudios.pureviews.circle.CircleMenu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by robwilliams on 2015-11-09.
@@ -19,11 +23,30 @@ public class MenuScreen extends AppScreen {
     private int mWidth = -1;
     private int mHeight = -1;
     private Paint mLinePaint;
+    private CircleMenu mCircleMenu;
 
     public MenuScreen(Context context) {
         super(context);
         init();
     }
+
+    private String [] mNames = new String[] { "albums", "playlist","genre","artist","songs" };
+    private int [] mResourceIds = new int [] { R.drawable.core_media_player_img_ic_albums, R.drawable.core_media_player_img_ic_playlist,
+    R.drawable.core_media_player_img_ic_genre, R.drawable.core_media_player_img_ic_artist, R.drawable.core_media_player_img_icon_128x128};
+
+    private CircleMenu.Configuration mConfiguration = new CircleMenu.Configuration() {
+        private ArrayList<CircleMenu.CircleMenuItem> mItems = new ArrayList<>();
+
+        @Override
+        public List<CircleMenu.CircleMenuItem> getItems() {
+            if (mItems.size()==0) {
+                for (int i=0; i<mNames.length; ++i) {
+                    mItems.add(new CircleMenu.CircleMenuItem(mNames[i],mResourceIds[i],null));
+                }
+            }
+            return mItems;
+        }
+    };
 
     public MenuScreen(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -47,6 +70,9 @@ public class MenuScreen extends AppScreen {
         mLinePaint.setAlpha(255);
         mLinePaint.setStrokeWidth(2);
         mLinePaint.setStyle(Paint.Style.STROKE);
+
+        mCircleMenu = new CircleMenu(getContext(),mConfiguration);
+        addView(mCircleMenu);
     }
 
     @Override
@@ -60,6 +86,8 @@ public class MenuScreen extends AppScreen {
 
         mWidth = width;
         mHeight = height;
+
+        mCircleMenu.layout(0, 40, width, height);
     }
 
     @Override
