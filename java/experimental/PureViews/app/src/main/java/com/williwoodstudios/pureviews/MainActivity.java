5104d05cabbefb1baacc631403748a5fcd50efeb
Rob Williams <rob@workshoptwelve.com>
2015-11-13 11:44:07 -0500

    Added network service.

--- java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/MainActivity.java
+++ java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/MainActivity.java
@@ -1,6 +1,7 @@
 package com.williwoodstudios.pureviews;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.williwoodstudios.pureviews.networking.NetworkService;
 
 public class MainActivity extends Activity {
 
@@ -24,8 +27,14 @@ public class MainActivity extends Activity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(new BrainiacLandscapeLayout(this));
+
+        Intent intent = new Intent(this, NetworkService.class);
+        startService(intent);
     }
 
-
-
+    @Override
+    protected void onDestroy() {
+        Intent intent = new Intent(this, NetworkService.class);
+        stopService(intent);
+    }
 }
