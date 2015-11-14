6c1969095060cf52d8dad785a9c87731dea9a070
Rob Williams <rob@workshoptwelve.com>
2015-11-14 13:23:46 -0500

    Added round button. Changed ListView to be simple list.

--- java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/air/AirMainScreen.java
+++ java/experimental/PureViews/app/src/main/java/com/williwoodstudios/pureviews/air/AirMainScreen.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.williwoodstudios.pureviews.AppScreen;
 import com.williwoodstudios.pureviews.R;
+import com.williwoodstudios.pureviews.controls.RoundedRectButton;
+import com.williwoodstudios.pureviews.wedge.WedgeDialogScreen;
+
+import java.util.ArrayList;
 
 /**
  * Created by robwilliams on 2015-11-09.
@@ -19,6 +21,7 @@ public class AirMainScreen extends AppScreen {
     private int[][] mPresetValues = new int[][]{{20, 20, 25, 27, 160}, {40, 40, 42, 45, 160}, {72, 71, 76, 77, 160}};
 
     private int mSelectedPreset = -1;
+    private RoundedRectButton mAddSetButton;
 
     public int getSelectedPreset() {
         return mSelectedPreset;
@@ -26,12 +29,16 @@ public class AirMainScreen extends AppScreen {
 
     public void setSelectedPreset(int which) {
         if (which>=0 && which < mPresets.length) {
+            for (int i=0; i<mPresetViews.size(); ++i) {
+                mPresetViews.get(i).setSelected(which==i);
+            }
             mPressureControl.setPressures(mPresetValues[which]);
+            setButtonMode(Mode.ADD_CONDITION);
         }
     }
 
     private PressureControl mPressureControl;
-    private ListView mPressureList;
+
     private View mSpacer;
 
 
@@ -50,50 +57,74 @@ public class AirMainScreen extends AppScreen {
         init();
     }
 
-    protected void selectPreset(int which) {
-        if (which < 0 || which >= mPresets.length) {
-            // do nothing.
-        } else {
-            mPressureControl.setPressures(mPresetValues[which]);
+    enum Mode {
+        ADD_CONDITION,
+        SAVE_PRESET
+    };
+
+    private Mode mMode = null;
+
+    private ArrayList<PresetItemView> mPresetViews = new ArrayList<>();
+
+    private void setButtonMode(Mode mode) {
+        if (mode != mMode) {
+            mMode = mode;
+            if (mode == Mode.ADD_CONDITION) {
+                mAddSetButton.setText("Add Condition");
+            } else {
+                mAddSetButton.setText("Save Preset");
+                for (PresetItemView v : mPresetViews) {
+                    v.setSelected(false);
+                }
+            }
         }
     }
 
     private void init() {
-        setBackgroundColor(0xff007f00);
-        mPressureControl = new PressureControl(getContext(), this);
+        setBackgroundColor(0xff000000);
+        mPressureControl = new PressureControl(getContext(), this) {
+            @Override
+            void onPressuresChanged(int fl, int fr, int bl, int br) {
+                Log.e("AirMainScreen","Pressure changed");
+                setButtonMode(Mode.SAVE_PRESET);
+            }
+        };
         mPressureControl.setPressures(new int[]{40, 40, 42, 45, 162});
 
         addView(mPressureControl);
 
-        mPressureList = new ListView(getContext());
-        mPressureList.setAdapter(new PresetListAdapter(this));
-        mPressureList.setBackgroundColor(0xff000000);
-        mPressureList.setSelection(0);
-        mPressureList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        mPressureList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.e("AirMainScreen","Item click "+position+ " " + id);
-                view.setSelected(true);
-                selectPreset(position/2);
-            }
-        });
-//        mPressureList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                Log.e("AirMainScreen", "Item selected " + position);
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                Log.e("AirMainScreen","Nothing selected");
-//            }
-//        });
-        addView(mPressureList);
-
+        for (int i=0; i<mPresets.length; ++i) {
+            final int which = i;
+            PresetItemView toAdd = new PresetItemView(getContext());
+            toAdd.setText(mPresets[i]);
+            toAdd.setSelected(false);
+            toAdd.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setSelectedPreset(which);
+                }
+            });
+            mPresetViews.add(toAdd);
+            addView(toAdd);
+        }
         mSpacer = new View(getContext());
         mSpacer.setBackgroundColor(0xffffffff);
         addView(mSpacer);
+
+        mAddSetButton = new RoundedRectButton(getContext());
+        addView(mAddSetButton);
+        mAddSetButton.setText("Add Condition");
+
+        mAddSetButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WedgeDialogScreen wds = new WedgeDialogScreen(getContext());
+                wds.setPrompt("Activate Preset\n'Full Height'?");
+                pushScreen(wds);
+            }
+        });
+
+        setSelectedPreset(0);
     }
 
     @Override
@@ -105,19 +136,26 @@ public class AirMainScreen extends AppScreen {
             mWidth = width;
             mHeight = height;
 
+            int buttonVSpace = 60;
+
             mPressureControl.layout(width - height, 0, width, height);
 
-            mPressureList.layout(0, 0, width - height, height);
+            int presetVOffset = 0;
+            for (PresetItemView v : mPresetViews) {
+                v.measure(0,0);
+                v.layout(0,presetVOffset,width-height-1,presetVOffset+v.getMeasuredHeight());
+                presetVOffset += v.getMeasuredHeight();
+            }
+
+            int buttonPad = 8;
+
+            mAddSetButton.layout(buttonPad,height-buttonVSpace,width-height-buttonPad,height-buttonPad);
 
             mSpacer.layout(width - height - 1, 0, width - height, height);
         }
     }
 
     private int mWidth, mHeight;
-
-    public String[] getPresets() {
-        return mPresets;
-    }
 
     @Override
     public int getNavigationLevel() {
