cb89a42c721e975879c0392c10894a98504aa192
Rob Williams <rob@workshoptwelve.com>
2015-12-22 16:24:15 -0500

    Day of research!

--- firmware/experiments/sniffers/airLift/autoPilot/serialReader.py
+++ firmware/experiments/sniffers/airLift/autoPilot/serialReader.py
@@ -3,20 +3,32 @@ import time
 
 ser = serial.Serial('/dev/cu.usbmodem1411',115200)
 
-lineBuffer = range(0,16)
+lineBuffer = range(0,32)
 lineOffset = 0
 lineFrom = -1
 lineLastByteTime = 0
+lineFirstByteTime = 0
 
 def flushLine():
     global lineBuffer
     global lineOffset
     global lineFrom
+    global lineFirstByteTime
 
     if lineOffset == 0:
         return
 
     message = ""
+
+    when = lineFirstByteTime
+    when = when % 86400
+    hours = int(when) / 3600
+    minutes = (int(when) / 60) % 60
+    seconds = (int(when)) % 60
+    frac = int((when - int(when)) * 1000)
+
+    message = "%02d:%02d:%02d.%03d: " % (hours, minutes, seconds, frac)
+
 
     if lineFrom == 0:
         message = message + "> "
@@ -30,9 +42,8 @@ def flushLine():
 
     print message
 
-
-
     lineOffset = 0
+    lineFirstByteTime = 0
 
 while True:
     a = 0
@@ -48,17 +59,23 @@ while True:
     if a != lineFrom:
         flushLine()
 
+    if val == 0xfa:
+        flushLine()
+
     now = time.time()
 
-    if lineOffset > 0 and now - lineLastByteTime > 0.1:
-        flushLine()
+    if lineFirstByteTime == 0:
+        lineFirstByteTime = now
+
+    # if lineOffset > 0 and now - lineLastByteTime > 0.5:
+    #     flushLine()
 
     lineLastByteTime = now
     lineFrom = a
     lineBuffer[lineOffset] = val
     lineOffset = lineOffset + 1
 
-    if lineOffset == 16:
+    if lineOffset == 32:
         flushLine()
 
 
